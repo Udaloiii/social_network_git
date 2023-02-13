f89dc2cc2dd7a4a34cf115c7324c5a55eae23bf2
Udaloiii <udaloiiisohappy@gmail.com>
2023-02-13 19:45:30 +0300

    Add startpage and classnames

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go! Just do it!)
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className= "header">
+                <img
+                    src="https://external-preview.redd.it/T1aS95K6uq9RQwYH5KGvVmqb6_XfsGIXd_XpcavN43o.png?width=640&crop=smart&auto=webp&v=enabled&s=87175ef7a41ecb39ebf5e9d9f2809dc6b788a080"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    Profile
+                </div>
+            </nav>
+
+            <div className="content">
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
